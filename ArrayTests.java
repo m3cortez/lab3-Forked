984654117b8f7a75c38546e0b1431c55dd207a30
Matcor263 <122655882+Matcor263@users.noreply.github.com>
2023-10-30 12:21:22 -0700

    Bug Fixes

--- ArrayTests.java
+++ ArrayTests.java
@@ -3,15 +3,42 @@ import org.junit.*;
 
 public class ArrayTests {
 	@Test 
-	public void testReverseInPlace() {
+	public void testReverseInPlaceOne() {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test 
+	public void testReverseInPlaceReg() {
+    int[] input1 = {3,2,1};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{1,2,3}, input1);
+	}
 
   @Test
-  public void testReversed() {
+  public void testReverseInPlaceEmpty() {
+    int[] input1 = { };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ }, input1);
+  }
+
+  //--------------------------------------------------
+
+  @Test 
+	public void testReversedOne() {
+    int[] input1 = { 3 };
+    assertArrayEquals(new int[]{ 3 }, ArrayExamples.reversed(input1));
+	}
+
+  @Test 
+	public void testReversedReg() {
+    int[] input1 = {3,2,1};
+    assertArrayEquals(new int[]{1,2,3}, ArrayExamples.reversed(input1));
+	}
+
+  @Test
+  public void testReversedEmpty() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
